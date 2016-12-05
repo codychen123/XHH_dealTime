55ee0722bf25ded22be815ef8db7e7de645d35eb
ithuihui <chhhithuihui@gmail.com>
2016-12-05 11:04:23 +0800

    Initial Commit

---
+++ dealTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dealTime
+//
+//  Created by xiaohuihui on 2016/12/5.
+//  Copyright © 2016年 30-陈辉华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dealTime/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  dealTime
+//
+//  Created by xiaohuihui on 2016/12/5.
+//  Copyright © 2016年 30-陈辉华. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
